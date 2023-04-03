bf8013723f39c376d21553150c3f6248df6e8558
paradox-11 <21009201315@stu.xidian.edu.cn>
2023-04-03 15:26:11 +0800

    1.0

--- doe_final.py
+++ doe_final.py
@@ -7,9 +7,9 @@ import torch.backends.cudnn as cudnn
 import torchvision.transforms as trn
 import torchvision.datasets as dset
 import torch.nn.functional as F
+import torch.nn as nn
 
 from models.wrn import WideResNet
-from models.allconv import AllConvNet
 import torchvision.models as models
 import utils.utils_awp as awp
 from utils.display_results import  get_measures, print_measures
@@ -52,6 +52,10 @@ parser.add_argument('--seed', type=int, default=1, help='seed for np(tinyimages8
 parser.add_argument('--warmup', type=int, default=5)
 parser.add_argument('--begin_epoch', type=int, default=0)
 
+parser.add_argument('--model', type=str, choices=['wrn', 'resnet18', 'resnet50'])
+parser.add_argument('--lr_adjust', type=str, choices=['cos', 'poem'])
+parser.add_argument('--oe_setting', type=str, help='l + 0.5 * loe')
+
 torch.cuda.set_device(0) # 选择第0张卡
 # torch.cuda.set_device(1) # 选择第1张卡
 
@@ -60,12 +64,20 @@ torch.manual_seed(1)
 np.random.seed(args.seed)
 torch.cuda.manual_seed(1)
 
-log = logging.getLogger("cifar10_resnet50.log")
-fileHandler = logging.FileHandler("./logging/cifar10_resnet50.log", mode='a')
-log.setLevel(logging.DEBUG)
-log.addHandler(fileHandler)
-log.debug("cifar10_resnet50.log")
-log.debug("")
+if args.oe_setting == "yes":
+    log = logging.getLogger(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + "_oe" + ".log")
+    fileHandler = logging.FileHandler(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + "_oe" + ".log", mode='a')
+    log.setLevel(logging.DEBUG)
+    log.addHandler(fileHandler)
+    log.debug(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + "_oe" + ".log")
+    log.debug("")
+else:
+    log = logging.getLogger(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + ".log")
+    fileHandler = logging.FileHandler(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + ".log", mode='a')
+    log.setLevel(logging.DEBUG)
+    log.addHandler(fileHandler)
+    log.debug(args.dataset + "_" + args.model + "_" + str(args.learning_rate) + "_" + args.lr_adjust + ".log")
+    log.debug("")
 for k, v in args._get_kwargs():
     log.debug(str(k)+": "+str(v))
 log.debug("")
@@ -155,15 +167,10 @@ def get_and_print_results(mylog, ood_loader, in_score, num_to_avg=args.num_to_av
     return fpr, auroc, aupr
 
 def train(epoch, diff):
+    if args.lr_adjust == 'poem':
+        adjust_learning_rate(optimizer, epoch) # 修改
     
-    # adjust_learning_rate(optimizer, epoch) # POEM中使用的学习率调整方法 修改
-    
-    # proxy = WideResNet(args.layers, num_classes, args.widen_factor, dropRate = 0).cuda()
-    # proxy = AllConvNet(num_classes).cuda() # 修改
-    # proxy = models.resnet18() # 修改
-    proxy = models.resnet50() # 修改
-    proxy.fc = torch.nn.Linear(in_features=proxy.fc.in_features, out_features=10, bias=True) # 修改
-    proxy.cuda() # 修改
+    proxy = WideResNet(args.layers, num_classes, args.widen_factor, dropRate = 0).cuda()
     proxy_optim = torch.optim.SGD(proxy.parameters(), lr=1) # 辅助模型
 
     net.train()
@@ -185,14 +192,13 @@ def train(epoch, diff):
             x = proxy(data) * scale
             l_sur = (x[len(in_set[0]):].mean(1) - torch.logsumexp(x[len(in_set[0]):], dim=1)).mean() # 这里求的是min Loe, 所以会差一个负号
             # l_sur = - (x.log_softmax(1) * (x / 0.1).softmax(1).detach()).sum(-1).mean()
-            emb = proxy.intermediate_forward_simple(data) # 修改!
-            reg_sur = torch.sum(torch.autograd.grad(l_sur, [scale], create_graph = True)[0] ** 2) + torch.norm(emb, 1) # 修改! 计算l_sur关于scale的梯度，并将其平方后求和
+            # emb = proxy.intermediate_forward_simple(data) # 修改!
+            # reg_sur = torch.sum(torch.autograd.grad(l_sur, [scale], create_graph = True)[0] ** 2) + torch.norm(emb, 1) # 修改! 计算l_sur关于scale的梯度，并将其平方后求和
+            reg_sur = torch.sum(torch.autograd.grad(l_sur, [scale], create_graph = True)[0] ** 2)
             proxy_optim.zero_grad()
             reg_sur.backward()
             # l_sur.backward()
-            torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作(test2)
-            # torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1, norm_type=1) # 修改
-            # torch.nn.utils.clip_grad_norm_(proxy.parameters(), 1) # 修改
+            torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作
             proxy_optim.step()
             if epoch == args.warmup and batch_idx == 0:
                 diff = awp.diff_in_weights(net, proxy) # 微分操作(第一次还无法加权平均)
@@ -214,7 +220,6 @@ def train(epoch, diff):
                 #结合DRO
 
             awp.add_into_weights(net, diff, coeff = gamma)
-            # awp.add_into_weights(net, diff, coeff = - gamma) # 修改
 
         #结合DRO
         # gamma upgrade
@@ -227,7 +232,10 @@ def train(epoch, diff):
         # pdb.set_trace()
         x = net(data)
         l_ce = F.cross_entropy(x[:len(in_set[0])], target)
-        l_oe = - (x[len(in_set[0]):].mean(1) - torch.logsumexp(x[len(in_set[0]):], dim=1)).mean()
+        if args.oe_setting == "yes":
+            l_oe = 0.5 * - (x[len(in_set[0]):].mean(1) - torch.logsumexp(x[len(in_set[0]):], dim=1)).mean() # 修改
+        else:
+            l_oe = - (x[len(in_set[0]):].mean(1) - torch.logsumexp(x[len(in_set[0]):], dim=1)).mean()
         if args.dataset == 'cifar10':
             if epoch >= args.warmup:
                 loss = l_oe
@@ -241,25 +249,23 @@ def train(epoch, diff):
             
         optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作(test3)
-        # torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1, norm_type=1) # 修改
+        torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作
         optimizer.step()
 
         if epoch >= args.warmup:
             awp.add_into_weights(net, diff, coeff = - gamma)
-            # awp.add_into_weights(net, diff, coeff = gamma) # 修改
             optimizer.zero_grad()
             x = net(data)
             l_ce = F.cross_entropy(x[:len(in_set[0])], target)
             loss = l_ce # + l_kl
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作(test3)
-            # torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1, norm_type=1) # 修改
+            torch.nn.utils.clip_grad_norm_(net.parameters(), 1) # norm操作
             optimizer.step()
 
         loss_avg = loss_avg * 0.8 + float(loss) * 0.2
         sys.stdout.write('\r epoch %2d %d/%d loss %.2f' %(epoch, batch_idx + 1, len(train_loader_in), loss_avg))
-        scheduler.step()
+        if args.lr_adjust == 'cos':
+            scheduler.step()
 
     print()
     return diff
@@ -277,19 +283,37 @@ def test():
     return correct / len(test_loader.dataset) * 100
 
 print('Beginning Training\n')
-# net = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate).cuda()
-# net = AllConvNet(num_classes) # 修改
-# net = models.resnet18() # 修改
-net = models.resnet50() # 修改
-net.fc = torch.nn.Linear(in_features=net.fc.in_features, out_features=10, bias=True) # 修改
-net.cuda() # 修改
 # Restore model
-# if args.dataset == 'cifar10':
-#     model_path = './ckpt/cifar10_wrn_pretrained_epoch_99.pt'
-# else:
-#     model_path = './ckpt/cifar100_wrn_pretrained_epoch_99.pt'
-# model_path = './ckpt/resnet18_cifar10_epoch100.pt' # 修改
-model_path = './ckpt/resnet50_cifar10_epoch100.pt' # 修改
+if args.dataset == 'cifar10':
+    if args.model == 'wrn':
+        net = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate)
+        model_path = './ckpt/cifar10_wrn_pretrained_epoch_99.pt'
+    elif args.model == 'resnet18':
+        net = models.resnet18()
+        net.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        net.fc = torch.nn.Linear(in_features=net.fc.in_features, out_features=num_classes, bias=True)
+        model_path = './ckpt/resnet18_cifar10_epoch147.pt'
+    elif args.model == 'resnet50':
+        net = models.resnet50()
+        net.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        net.fc = torch.nn.Linear(in_features=net.fc.in_features, out_features=num_classes, bias=True)
+        model_path = './ckpt/resnet50_cifar10_epoch124.pt'
+elif args.dataset == 'cifar100':
+    if args.model == 'wrn':
+        net = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate).cuda()
+        model_path = './ckpt/cifar100_wrn_pretrained_epoch_99.pt'
+    elif args.model == 'resnet18':
+        net = models.resnet18()
+        net.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        net.fc = torch.nn.Linear(in_features=net.fc.in_features, out_features=num_classes, bias=True)
+        model_path = './ckpt/resnet18_cifar100_epoch109.pt'
+    elif args.model == 'resnet50':
+        net = models.resnet50()
+        net.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        net.fc = torch.nn.Linear(in_features=net.fc.in_features, out_features=num_classes, bias=True)
+        model_path = './ckpt/resnet50_cifar100_epoch170.pt'
+
+net.cuda()
 net.load_state_dict(torch.load(model_path))
 optimizer = torch.optim.SGD(net.parameters(), args.learning_rate, momentum=args.momentum, weight_decay=args.decay, nesterov=True)
 
@@ -307,35 +331,37 @@ def adjust_learning_rate(optimizer, epoch, lr_schedule=[4, 6, 8]): # POEM中使�
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda step: cosine_annealing(step, args.epochs * len(train_loader_in), 1, 1e-6 / args.learning_rate))
+if args.lr_adjust == 'cos':
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda step: cosine_annealing(step, args.epochs * len(train_loader_in), 1, 1e-6 / args.learning_rate))
 diff = None
 
-# for epoch in range(args.begin_epoch, args.epochs - 2): # 修改
-for epoch in range(args.begin_epoch, args.epochs - 1):
-# for epoch in range(args.begin_epoch, args.epochs): # 修改
-    diff = train(epoch, diff)
+def evaluate():
+    net.eval()
+    print('  FPR95 AUROC AUPR (acc %.2f)' % test())
+    log.debug('  FPR95 AUROC AUPR (acc %.2f)' % test())
+    in_score = get_ood_scores(test_loader, in_dist=True)
+    metric_ll = []
+    print('lsun')
+    log.debug('lsun')
+    metric_ll.append(get_and_print_results(log, lsunc_loader, in_score))
+    print('isun')
+    log.debug('isun')
+    metric_ll.append(get_and_print_results(log, isun_loader, in_score))
+    print('texture')
+    log.debug('texture')
+    metric_ll.append(get_and_print_results(log, texture_loader, in_score))
+    print('places')
+    log.debug('places')
+    metric_ll.append(get_and_print_results(log, places365_loader, in_score))
+    print('total')
+    log.debug('total')
+    print('& %.2f & %.2f & %.2f' % tuple((100 * torch.Tensor(metric_ll).mean(0)).tolist()))
+    log.debug('& %.2f & %.2f & %.2f' % tuple((100 * torch.Tensor(metric_ll).mean(0)).tolist()))
+    print('cifar')
+    log.debug('cifar')
+    get_and_print_results(log, cifar_loader, in_score)
 
-net.eval()
-print('  FPR95 AUROC AUPR (acc %.2f)' % test())
-log.debug('  FPR95 AUROC AUPR (acc %.2f)' % test())
-in_score = get_ood_scores(test_loader, in_dist=True)
-metric_ll = []
-print('lsun')
-log.debug('lsun')
-metric_ll.append(get_and_print_results(log, lsunc_loader, in_score))
-print('isun')
-log.debug('isun')
-metric_ll.append(get_and_print_results(log, isun_loader, in_score))
-print('texture')
-log.debug('texture')
-metric_ll.append(get_and_print_results(log, texture_loader, in_score))
-print('places')
-log.debug('places')
-metric_ll.append(get_and_print_results(log, places365_loader, in_score))
-print('total')
-log.debug('total')
-print('& %.2f & %.2f & %.2f' % tuple((100 * torch.Tensor(metric_ll).mean(0)).tolist()))
-log.debug('& %.2f & %.2f & %.2f' % tuple((100 * torch.Tensor(metric_ll).mean(0)).tolist()))
-print('cifar')
-log.debug('cifar')
-get_and_print_results(log, cifar_loader, in_score)
+# for epoch in range(args.begin_epoch, args.epochs): # 修改
+for epoch in range(args.begin_epoch, args.epochs - 1):
+    diff = train(epoch, diff)
+    evaluate()
